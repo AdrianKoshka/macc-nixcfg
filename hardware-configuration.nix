d9b323863aa54c9707cfb37f00c371c0b2daa906
Adrian Lucrèce Céleste <adrian.lucrece.celeste@ibm.com>
2021-06-27 15:24:40 -0400

    Set CPU governor to performance

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -23,16 +23,7 @@
       fsType = "vfat";
     };
 
-  swapDevices = [ 
-   { 
-    device = "/dev/disk/by-partuuid/fc8c8b70-a3a8-0e47-a57d-0f67100ac270";
-    randomEncryption = {
-     cipher = "aes-xts-plain64";
-     enable = true;
-     source = "/dev/random";
-    };
-   }
-  ];
+  swapDevices = [ ];
 
-  powerManagement.cpuFreqGovernor = lib.mkDefault "ondemand";
+  powerManagement.cpuFreqGovernor = lib.mkDefault "performance";
 }
