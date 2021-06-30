173afcbdc8e1b8c626bb5d76b5d64fca5aa88299
Adrian Lucrèce Céleste <adrian.lucrece.celeste@ibm.com>
2021-06-30 13:18:56 -0400

    reorganize and make vim the default editor

--- misc.nix
+++ misc.nix
@@ -13,9 +13,15 @@
 
  # Some programs need SUID wrappers, can be configured further or are
  # started in user sessions.
- programs.mtr.enable = true;
- programs.gnupg.agent = {
+ programs = {
+  mtr.enable = true;
+  gnupg.agent = {
    enable = true;
    enableSSHSupport = true;
+  };
+  bash = {
+   shellInit = "EDITOR=vim";
+  };
+  vim.defaultEditor = true;
  };
 }
