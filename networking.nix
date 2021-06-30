e7d5c5c859034e8f7aa56b915c65fef9f32c830d
Adrian Lucrèce Céleste <adrian.lucrece.celeste@ibm.com>
2021-06-30 16:09:12 -0400

    Add virtual function interfaces to list of NICs to not use DHCP

--- networking.nix
+++ networking.nix
@@ -13,6 +13,12 @@
     enp0s0f1.useDHCP = false;
     enp0s0f2.useDHCP = false;
     enp0s0f3.useDHCP = false;
+    enp0s0f0v1.useDHCP = false;
+    enp0s0f0v2.useDHCP = false;
+    enp0s0f0v3.useDHCP = false;
+    enp0s0f0v4.useDHCP = false;
+    enp0s0f0v5.useDHCP = false;
+    enp0s0f0v6.useDHCP = false;
     eth4.useDHCP = false;
     eth5.useDHCP = false;
     eth6.useDHCP = false;
