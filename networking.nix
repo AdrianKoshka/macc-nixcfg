16eb52d6f21ee4171a6ea0d8be1578bb525933e4
Adrian Lucrèce Céleste <adrian.lucrece.celeste@ibm.com>
2021-07-13 16:29:15 -0400

    Remove NICs from list

--- networking.nix
+++ networking.nix
@@ -3,6 +3,7 @@
 {
  networking = {
   hostName = "macc"; # Define your hostname.
+  domain = "home.arpa";
   useDHCP = false;
   firewall.enable = false;
   networkmanager = {
@@ -13,14 +14,11 @@
     enp0s0f1.useDHCP = false;
     enp0s0f2.useDHCP = false;
     enp0s0f3.useDHCP = false;
-    enp0s0f0v1.useDHCP = false;
     enp0s0f0v2.useDHCP = false;
     enp0s0f0v3.useDHCP = false;
     enp0s0f0v4.useDHCP = false;
     enp0s0f0v5.useDHCP = false;
     enp0s0f0v6.useDHCP = false;
-    eth4.useDHCP = false;
-    eth5.useDHCP = false;
     eth6.useDHCP = false;
     eth7.useDHCP = false;
   };
